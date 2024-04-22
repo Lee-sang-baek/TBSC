4bca8ffb9ee44d390613756bc19aab521c2262b7
lsb <dltkdqor11@gmail.com>
2024-04-22 14:24:14 +0900

    rename

---
+++ src/main/java/com/tbsc/TbscApplication.java
@@ -0,0 +1,13 @@
+package com.tbsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbsc/TbscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
