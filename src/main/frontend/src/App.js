395c09429bd84f856dfa7332646a2f0324013efd
해빈 <2haebin78@gmail.com>
2024-04-27 00:46:25 +0900

    Changes

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,14 +1,15 @@
-// App.js
 import React from 'react';
 import Header from './Components/Header/Header';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  return (
-      <div>
-          <Header />
-
-      </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Header />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/main/frontend/src/Components/Header/Header.js
+++ src/main/frontend/src/Components/Header/Header.js
@@ -1,39 +1,89 @@
 import React, { useState } from 'react';
-import './Header.css'; // CSS 파일 경로에 맞게 수정해주세요.
+import { Link } from 'react-router-dom';
+import './Header.css';
+import logoImage from '../Imgs/logo.png';
+import loginImage from '../Imgs/login.png';
 
 const Header = () => {
-    const [showDropdown, setShowDropdown] = useState(false);
+    const [showSubmenu, setShowSubmenu] = useState(false);
 
     const categories = [
-        { title: '광주관광기업지원센터', submenus: ['센터 소개', '시설안내', '조직도', '오시는길'] },
-        { title: '지원/운영사업', submenus: ['창업 및 성장지원', '교육 및 컨설팅', '관광일자리 지원'] },
-        { title: '입주기업홍보관', submenus: ['기업 소개', '기업 홍보'] },
-        { title: '센터 이용예약', submenus: ['이용예약 안내', '기업 컨설팅 신청', '일자리 상담신청', '회의실 대관 신청'] },
-        { title: '알림마당', submenus: ['공지사항', '센터소식', '보도자료'] },
+        {
+            title: '광주 관광사업 지원센터',
+            submenus: [
+                { name: '센터 소개', path: '/about' },
+                { name: '시설 안내', path: '/facilities' },
+                { name: '조직도', path: '/organization' },
+                { name: '오시는 길', path: '/directions' }
+            ]
+        },
+        {
+            title: '지원/운영 사업',
+            submenus: [
+                { name: '창업 및 성장 지원', path: '/startup-support' },
+                { name: '교육 및 컨설팅', path: '/education-consulting' },
+                { name: '관광 일자리 지원', path: '/tourism-jobs-support' }
+            ]
+        },
+        {
+            title: '기업 홍보관',
+            submenus: [
+                { name: '기업 소개', path: '/company-introduction' },
+                { name: '기업 홍보', path: '/company-promotion' }
+            ]
+        },
+        {
+            title: '센터 시설 예약',
+            submenus: [
+                { name: '예약 정보', path: '/reservation-info' },
+                { name: '기업 상담 신청', path: '/consulting-application' },
+                { name: '취업 상담 신청', path: '/job-consulting-application' },
+                { name: '회의실 신청', path: '/meeting-room-application' }
+            ]
+        },
+        {
+            title: '공지 사항',
+            submenus: [
+                { name: '공지사항', path: '/notices' },
+                { name: '센터 뉴스', path: '/news' },
+                { name: '보도자료', path: '/press-releases' }
+            ]
+        },
     ];
 
+    const handleMouseEnter = () => {
+        setShowSubmenu(true);
+    };
+
+    const handleMouseLeave = () => {
+        setShowSubmenu(false);
+    };
+
     return (
-        <nav className="navbar">
-            <div className="nav-categories-container" onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
-                <ul className="nav-categories">
-                    {categories.map((category, index) => (
-                        <li key={index}>{category.title}</li>
-                    ))}
-                </ul>
-                {showDropdown && <div className="dropdown-content">
-                    {/* title을 제거하고 submenus만 표시 */}
-                    {categories.map((category, index) => (
-                        <div key={index} className="dropdown-section">
-                            <div className="submenu-list">
-                                {category.submenus.map((submenu, i) => (
-                                    <div key={i}>{submenu}</div>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-                </div>}
+        <header>
+            <div className="logo-container">
+                <img src={logoImage} alt="Logo" className="logo"/>
+                <img src={loginImage} alt="Login" className="login"/>
             </div>
-        </nav>
+
+            <ul className="menu">
+            {categories.map((category, index) => (
+                    <li key={index} className="menu-item" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                        <Link to="/" className="menu-link">{category.title}</Link>
+                        <ul className={`submenu ${showSubmenu ? 'show' : ''}`}>
+                            {showSubmenu && category.submenus.map((submenu, subIndex) => (
+                                <li key={subIndex} className="submenu-item">
+                                    <Link to={submenu.path} className="submenu-link">{submenu.name}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </li>
+                ))}
+            </ul>
+            <div className="login-container">
+
+            </div>
+        </header>
     );
 };
 
