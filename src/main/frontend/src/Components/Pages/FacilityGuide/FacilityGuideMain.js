f0af418424b51334b1d42c4963504aef24bca4e8
lsb <dltkdqor11@gmail.com>
2024-05-09 11:21:56 +0900

    중간변경

--- src/main/frontend/src/Components/Pages/FacilityGuide/FacilityGuideMain.js
+++ src/main/frontend/src/Components/Pages/FacilityGuide/FacilityGuideMain.js
@@ -3,10 +3,15 @@ import FacilityGuide from "./FacilityGuide";
 import FacilityHeader from "./FacilityHeader";
 
 const FacilityGuideMain = () => {
+    const handlePlaceSelection = (placeName) => {
+        // 여기에서 placeName을 처리하는 로직
+        console.log("Selected Place:", placeName);
+    };
+
     return (
         <div>
             <FacilityHeader />
-            <FacilityGuide />
+            <FacilityGuide onSelectFacility={handlePlaceSelection} />
         </div>
     );
 }
