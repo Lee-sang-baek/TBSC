d91e41ff5fe1f9f2305cf56aaf613b9607d84752
Kim <tkdtlr97@naver.com>
2024-05-16 10:16:35 +0900

    리펙토링

--- src/main/frontend/src/components/fragments/mainHomepage/Banner.js
+++ src/main/frontend/src/components/fragments/mainHomepage/Banner.js
@@ -4,26 +4,26 @@ import Header from "./Components/Fragments/Header/Header";
 import Footer from "./Components/Fragments/Footer/Footer";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'; // 화살표 아이콘 import
+import {FaChevronLeft, FaChevronRight} from 'react-icons/fa'; // 화살표 아이콘 import
 import './Banner.css'; // CSS 파일 import
 
 function Banner() {
     // 이전 화살표 컴포넌트
     const PrevArrow = (props) => {
-        const { onClick } = props;
+        const {onClick} = props;
         return (
             <div className="arrow prev" onClick={onClick}>
-                <FaChevronLeft />
+                <FaChevronLeft/>
             </div>
         );
     };
 
     // 다음 화살표 컴포넌트
     const NextArrow = (props) => {
-        const { onClick } = props;
+        const {onClick} = props;
         return (
             <div className="arrow next" onClick={onClick}>
-                <FaChevronRight />
+                <FaChevronRight/>
             </div>
         );
     };
@@ -33,33 +33,34 @@ function Banner() {
         slidesToScroll: 1,
         autoplay: true, //자동 재생 여부
         autoplaySpeed: 2000, // 자동 재생 시 슬라이드 간의 간격(초) 지정
-        prevArrow: <PrevArrow />, // 이전 화살표 설정
-        nextArrow: <NextArrow /> // 다음 화살표 설정
+        prevArrow: <PrevArrow/>, // 이전 화살표 설정
+        nextArrow: <NextArrow/> // 다음 화살표 설정
     };
 
     return (
         <div>
-            <Header />
-        <div className="Banner-compo">
-        <div className="slider-container">
-            <Slider {...settings} className="autoplay">
-                <div>
-                    <img src="imgs/login.png" alt="image1" />
-                </div>
-                <div>
-                    <img src="imgs/logo.png" alt="image2" />
-                </div>
-                <div>
-                    <img src="/imgs/userProfile.png" alt="image3" />
-                </div>
+            <Header/>
+            <div className="Banner-compo">
+                <div className="slider-container">
+                    <Slider {...settings} className="autoplay">
+                        <div>
+                            <img src="imgs/login.png" alt="image1"/>
+                        </div>
+                        <div>
+                            <img src="imgs/logo.png" alt="image2"/>
+                        </div>
+                        <div>
+                            <img src="/imgs/userProfile.png" alt="image3"/>
+                        </div>
 
-            </Slider>
-        <Footer />
-            <div>
+                    </Slider>
+                    <Footer/>
+                    {/*<div>*/}
 
+                    {/*</div>*/}
+                </div>
+            </div>
         </div>
-        </div>
-
     );
 }
 
